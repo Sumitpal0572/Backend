e6e3d5fc289e1fab2d7538edbe296bb53f531d7a
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-10-22 23:40:23 +0530

    sets routes

--- Express-server/index.js
+++ Express-server/index.js
@@ -5,7 +5,28 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Express.js server")
 });
 
+app.get("/about", (req, res) => {
+    res.send("This is a simple web server build using Express.js");
+});
 
+app.get("/contact", (req, res) => {
+    res.json({
+        email: "student@example.com",
+        phone: "123-456-789",
+    });
+});
+
+app.get("/random", (req, res) => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    res.json({
+        randomNumber: randomNumber,
+    });
+});
+
+
+app.use((req, res) => {
+    res.status(404).send("Page not found");
+});
 
 app.listen(3000, () => {
     console.log("Server up to running on port 3000");
