ae0b7e49559b3477ebad9ff39d68ca81ab831246
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-11-08 12:04:22 +0530

    multer middleware done

--- Middleware_maltar/src/index.js
+++ Middleware_maltar/src/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require("express")
-const multar = require("multar")
+const multer = require('multer')
 const path = require('path')
 const { v2: cloudinary } = require('cloudinary') //Import data from cloudinary file as key value pair
+const { error } = require('console')
+
+
 const app = express()
 
 const PORT = 5000;
@@ -13,14 +16,14 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_SECRET
 })
 
-const storage = multar.diskStorage({
+const storage = multer.diskStorage({
     destination: path.join(__dirname, '../uploads'),
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}_${file.originalname}`)
     }
 })
 
-const upload = multar({ storage })
+const upload = multer({ storage })
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
@@ -30,11 +33,11 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 
     try {
         if (!req.file) {
-            return res.status(400).json({ message: 'Not file' })
+            return res.status(400).json({ message: 'No file found' })
         }
 
         const result = await cloudinary.uploader.upload(req.file.path, {
-            folder: 'uploads'
+            folder: 'uploads',
         })
 
         res.status(200).json({
@@ -42,10 +45,10 @@ app.post('/upload', upload.single('file'), async (req, res) => {
             imageUrl: result.secure_url
         })
 
-    } catch (error) {
-        console.log(error);
+    } catch (e) {
+        console.log(`Error in uploading file:${e}`);
         res.status(500).json({
-            message: "internal server error"
+            message: "internal server error", error: e.message
         })
     }
 })
