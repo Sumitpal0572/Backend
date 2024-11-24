f434b1f428fdaceaa23420687c4b4d954a66654e
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-11-24 21:47:19 +0530

    online-library system

--- Mongose_relation/online_library/index.js
+++ Mongose_relation/online_library/index.js
@@ -1,9 +1,25 @@
 const express = require("express");
-const app = express()
+const bodyParser = require("body-parser");
+const connect = require("./config/db");
+
+const authorRoutes = require("./routes/authorRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const userRoutes = require("./routes/userRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
+const app = express();
+app.use(bodyParser.json());
 
 const PORT = 9090;
 
 
-app.listen(PORT, () => {
+//Routes
+app.use("/authors", authorRoutes);
+app.use("/books", bookRoutes);
+app.use("/users", userRoutes);
+app.use("transactions", transactionRoutes);
+
+app.listen(PORT, async () => {
+    await connect()
     console.log(`server running on port:${PORT}`);
 })
