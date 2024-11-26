631ed45a1f7799a0785abd98ed63d8f65a078347
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-11-26 22:59:39 +0530

    Event management system done

--- Mongose_relation/EventManagementSystem/index.js
+++ Mongose_relation/EventManagementSystem/index.js
@@ -1,8 +1,16 @@
 const express = require("express")
+const connect = require("./config/db");
 const app = express()
+const eventRoute = require("./routes/eventRoutes");
 
 const PORT = 5000
+app.use(express.json());
 
-app.listen(PORT, () => {
+
+//Routes
+app.use("/", eventRoute);
+
+app.listen(PORT,async () => {
+    connect()
     console.log(`Server running on Port:${PORT}`);
 }) 
