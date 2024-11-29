aa03960e446031fcae5e5e38791fe71db6f8d58e
Sumitpal0572 <asmyselfsumit@gmail.com>
2024-11-29 23:39:50 +0530

    db setup done

---
+++ FullStackApp-Backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const connect = require('./config/db')
+const app = express()
+
+app.listen(3000,async () => {
+    await connect();
+    console.log(`Server is running on port`);
+})
